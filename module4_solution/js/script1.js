df109da92c39e88a3fd439a9166668baea0a79de
mianrabia <mianrabiailyas8@gmail.com>
2025-08-20 11:22:35 +0500

    Update script1.js

--- module4_solution/js/script1.js
+++ module4_solution/js/script1.js
@@ -24,15 +24,41 @@ $(function () {
 (function (global) {
   var dc = {};
 
-  var homeHtml = "snippets/home-snippet.html";
+  var homeHtml = "
+$(function () {
+  // Same as document.addEventListener("DOMContentLoaded"...
+
+  // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
+  $("#navbarToggle").blur(function (event) {
+    var screenWidth = window.innerWidth;
+    if (screenWidth < 768) {
+      $("#collapsable-nav").collapse("hide");
+    }
+  });
+
+  // In Firefox and Safari, the click event doesn't retain the focus
+  // on the clicked button. Therefore, the blur event will not fire on
+  // user clicking somewhere else in the page and the blur event handler
+  // which is set up above will not be called.
+  // Refer to issue #28 in the repo.
+  // Solution: force focus on the element that the click event fired on
+  $("#navbarToggle").click(function (event) {
+    $(event.target).focus();
+  });
+});
+
+(function (global) {
+  var dc = {};
+
+  var homeHtml = "module4_solution/snippets/home-snippet.html";
   var allCategoriesUrl =
     "https://coursera-jhu-default-rtdb.firebaseio.com/categories.json";
-  var categoriesTitleHtml = "snippets/categories-title-snippet.html";
-  var categoryHtml = "snippets/category-snippet.html";
+  var categoriesTitleHtml = "module4_solution/snippets/categories-title-snippet.html";
+  var categoryHtml = "module4_solution/snippets/category-snippet.html";
   var menuItemsUrl =
     "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/";
-  var menuItemsTitleHtml = "snippets/menu-items-title.html";
-  var menuItemHtml = "snippets/menu-item.html";
+  var menuItemsTitleHtml = "module4_solution/snippets/menu-items-title.html";
+  var menuItemHtml = "module4_solution/snippets/menu-item.html";
 
   // Convenience function for inserting innerHTML for 'select'
   var insertHtml = function (selector, html) {
@@ -43,7 +69,7 @@ $(function () {
   // Show loading icon inside element identified by 'selector'.
   var showLoading = function (selector) {
     var html = "<div class='text-center'>";
-    html += "<img src='images/ajax-loader.gif'></div>";
+    html += "<img src='module4_solution/images/ajax-loader.gif'></div>";
     insertHtml(selector, html);
   };
 
@@ -263,3 +289,4 @@ $(function () {
 
   global.$dc = dc;
 })(window);
+
