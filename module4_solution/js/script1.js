7fc29f5d9a21677e62b3f0f0f39dd32854dd4633
mianrabia <mianrabiailyas8@gmail.com>
2025-08-20 15:17:30 +0500

    Update script1.js

--- module4_solution/js/script1.js
+++ module4_solution/js/script1.js
@@ -24,32 +24,6 @@ $(function () {
 (function (global) {
   var dc = {};
 
-  var homeHtml = "
-$(function () {
-  // Same as document.addEventListener("DOMContentLoaded"...
-
-  // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
-  $("#navbarToggle").blur(function (event) {
-    var screenWidth = window.innerWidth;
-    if (screenWidth < 768) {
-      $("#collapsable-nav").collapse("hide");
-    }
-  });
-
-  // In Firefox and Safari, the click event doesn't retain the focus
-  // on the clicked button. Therefore, the blur event will not fire on
-  // user clicking somewhere else in the page and the blur event handler
-  // which is set up above will not be called.
-  // Refer to issue #28 in the repo.
-  // Solution: force focus on the element that the click event fired on
-  $("#navbarToggle").click(function (event) {
-    $(event.target).focus();
-  });
-});
-
-(function (global) {
-  var dc = {};
-
   var homeHtml = "module4_solution/snippets/home-snippet.html";
   var allCategoriesUrl =
     "https://coursera-jhu-default-rtdb.firebaseio.com/categories.json";
@@ -289,4 +263,3 @@ $(function () {
 
   global.$dc = dc;
 })(window);
-
